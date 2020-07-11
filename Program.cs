0a72ca7696ab0eb254c9bcb3c3ffcafc5b607312
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-11 10:32:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение, которое вы хотите вывести на экран.");
+            string codMessage = Console.ReadLine();
+            Console.Write("Введите количество выводимых сообщений:");
+            int amountMessages = Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<amountMessages;i++)
+            {
+                Console.WriteLine(codMessage);
+            }
+        }
+    }
+}
